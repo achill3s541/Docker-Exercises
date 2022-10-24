f687aa1f4bb5e19c954828fab067cb79512e48f9
achill3s541 <mj.pozyczka@gmail.com>
2022-10-24 14:50:08 +0200

    Exercise 3 has been added

---
+++ Dockerfile projects/Exercise 3/src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var students [5]string
+	fmt.Printf("Hello,\nI am simple GOlang app. I have been created to print some values from my array.")
+	fmt.Printf("Grades: %v \n", students)
+	students[0] = "Alice"
+	students[1] = "Lisa"
+	students[2] = "Johnny"
+	students[3] = "Owen"
+	students[4] = "Sara"
+	fmt.Printf("Students: %v \n", students)
+	fmt.Printf("Students: %v \n", students[4])
+	fmt.Printf("Number of students: %v\n", len(students))
+}
