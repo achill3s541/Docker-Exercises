f489b3b0fee803baaefe4b8a1cedd97410c53a55
achill3s541 <mj.pozyczka@gmail.com>
2023-02-08 11:31:09 +0100

    Init

---
+++ Dockerfile projects/PriceTracker/src/go.mod
@@ -0,0 +1,9 @@
+module github.com/achill3s541/PriceTracker
+
+go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.5.0 // indirect
+)
---
+++ Dockerfile projects/PriceTracker/src/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export envEmailSenderUser=<email_address>
+export envEmailSenderPassword=<password_to_email_address>
+export envEmailReceiver=<email_address>
+export envURLAddress=<url_address(zooplus.pl only)>
+cd /app/
+/app/tracker.app
